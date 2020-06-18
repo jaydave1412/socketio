fddf897bd1dbdcf88ffe5f1b5f502ca0ef3f2577
jay <davejay1412@gmail.com>
2020-06-18 23:11:12 +0530

    app finished

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+let socket = require('socket.io');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("hello")
+
+app.use(express.static('public'))
+// app.get('/',(req,res)=>{
+//     res.send("hello")
+// })
+
+const server = app.listen(3000,()=>{
+    console.log("server running")
 })
 
-app.listen(3000,()=>{
-    console.log("server running")
+let io = socket(server);
+
+io.on('connection',(socket)=>{
+    console.log("connected",socket.id);
+    socket.on('chat',(data)=>{
+        console.log(data)
+        io.sockets.emit('chat',data);
+    });
+
+    socket.on('typing',(data)=>{
+        socket.broadcast.emit('typing',data);
+    })
+
 })
